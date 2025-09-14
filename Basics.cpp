5d55da46693ae6c6512ff3d33488e8ca0ee8ee19
Adhyayan Das <167677739+Adhyayan96@users.noreply.github.com>
2025-09-14 15:01:13 +0530

    Here few topics of C++ I learnt today.
    Topics are :
    Variables & Datatypes
    Operators

---
+++ Basics.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// Printing mine and mother's name
+cout <<"Hello" << ", " << "Adhyayan Das\n" << "Jamuna Das\n";
+// Devision
+    int a;
+    float b;
+    cin >> a;
+    cin >> b;
+cout << a/b << "\n";
+// NOT operator use case
+cout << !(2<3) << "\n";
+// Finding out sum of 2 numbers
+int c;
+int d;
+cin >> c;
+cin >> d;
+cout << "Sum of these 2 numbers will be :" << c+d << "\n";
+// Using Unary Operators
+cout << a++ << "\n";
+cout << b-- << "\n";
+cout << ++a << "\n";
+cout << --b << "\n";
+return 0;
+}
