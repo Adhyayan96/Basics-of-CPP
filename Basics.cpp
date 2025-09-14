ea730b5371754bf033268124438a1d9db7b28be1
Atanu Debnath <138103949+atanu2005-ai@users.noreply.github.com>
2025-09-14 16:05:27 +0530

    Update Basics.cpp
    Added some user instructions and organised the code for a better look.

--- Basics.cpp
+++ Basics.cpp
@@ -1,27 +1,34 @@
+
 #include <iostream>
 using namespace std;
 
 int main() {
-// Printing mine and mother's name
-cout <<"Hello" << ", " << "Adhyayan Das\n" << "Jamuna Das\n";
-// Devision
+    // Printing mine and mother's name
+    cout <<"Hello" << ", " << "Adhyayan Das\n" << "Jamuna Das\n";
+    // Devision
     int a;
     float b;
+    cout << "Enter a number: ";
     cin >> a;
+    cout << "Enter another number: ";
     cin >> b;
-cout << a/b << "\n";
-// NOT operator use case
-cout << !(2<3) << "\n";
-// Finding out sum of 2 numbers
-int c;
-int d;
-cin >> c;
-cin >> d;
-cout << "Sum of these 2 numbers will be :" << c+d << "\n";
-// Using Unary Operators
-cout << a++ << "\n";
-cout << b-- << "\n";
-cout << ++a << "\n";
-cout << --b << "\n";
-return 0;
+    cout <<"Division of " << a << " by " << b << " will be: " << a/b << "\n";
+
+    // NOT operator use case
+    cout << !(2<3) << "\n";
+
+    // Finding out sum of 2 numbers
+    int c;
+    int d;
+    cout << "Enter a number: ";
+    cin >> c;
+    cout << "Enter another number: ";
+    cin >> d;
+    cout << "Sum of these 2 numbers will be :" << c+d << "\n";
+    // Using Unary Operators
+    cout << a++ << "\n";
+    cout << b-- << "\n";
+    cout << ++a << "\n";
+    cout << --b << "\n";
+    return 0;
 }
