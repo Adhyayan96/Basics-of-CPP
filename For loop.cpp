0a19d664fe8f851496d0ef8263c199f1dd78334c
Adhyayan96 <adhyayandas211@gmail.com>
2025-09-17 19:19:24 +0530

    learnt for loops

---
+++ For loop.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // in for loop : for (initialization; condition; update) {} - this way it works
+    for(int i = 1; i<=5; i+=1) {
+        cout << "Adhyayan" << "\n";
+    }
+return 0;
+}
