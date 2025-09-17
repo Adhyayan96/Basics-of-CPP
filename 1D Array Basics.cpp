a106e6ed2afd4a75a22cbed4048f27c66c084b10
Adhyayan96 <adhyayandas211@gmail.com>
2025-09-17 14:06:13 +0530

    learnt basic implementation of 1D, 2D Arrays & Strings

--- 1D Array Basics.cpp
+++ 1D Array Basics.cpp
@@ -6,7 +6,7 @@ int main() {
 int arr [5];
 cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
 cout << arr[3] << "\n";
-// Modifying an array
+// Modifying an array 
 arr [2] += 10;
 cout << arr[2] << "\n";
 return 0;
